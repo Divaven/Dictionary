bed557fed2f7f93b11302a6d259c600b1c6989d8
Arseniy Grazhdantsev <67071548+Divaven@users.noreply.github.com>
2025-01-25 09:17:56 +0300

    Update MVC

--- watch/watchers.js
+++ watch/watchers.js
@@ -2,7 +2,7 @@ import WatchJS from './melanke-watchjs.js';
 import model from '../js/model.js';
 import view from '../js/view.js';
 
-const { watch } = WatchJS;
+const {watch} = WatchJS;
 
 function watchState() {
     watch(model.state, 'dictionary', () => {
@@ -10,13 +10,26 @@ function watchState() {
         view.renderDictionaryList(model.state.dictionary);
     });
 
-    watch(model.state, 'currentWord', () => {
-        console.log('currentWord изменился на:', model.state.currentWord);
-    });
-
     watch(model.state, 'currentTranslation', () => {
         view.renderTranslation(model.state.currentTranslation);
+        view.setAddButtonState(true);
+    });
+
+    watch(model.state, 'currentWord', () => {
+        if (!model.state.currentWord) {
+            view.setAddButtonState(false);
+            view.clearTranslation();
+        }
+    });
+
+    watch(model.state, 'errorMessage', () => {
+        if (model.state.errorMessage) {
+            view.renderError(model.state.errorMessage);
+            view.setAddButtonState(false);
+        } else {
+            view.clearTranslation();
+        }
     });
 }
 
-export default watchState;
+export {watchState};
